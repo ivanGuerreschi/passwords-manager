466b952bea8b03d83dae121627d74182079d514a
Ivan Guerreschi <ivanguerreschi86@gmail.com>
2019-11-06 14:54:39 +0100

    Initial setup
    Initial setup quarkus and angular

---
+++ src/test/java/it/ivanguerreschi/passwordsmanager/NativePasswordResourceIT.java
@@ -0,0 +1,9 @@
+package it.ivanguerreschi.passwordsmanager;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativePasswordResourceIT extends PasswordResourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ src/test/java/it/ivanguerreschi/passwordsmanager/PasswordResourceTest.java
@@ -0,0 +1,21 @@
+package it.ivanguerreschi.passwordsmanager;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class PasswordResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/api")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
