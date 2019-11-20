bd7660c9437928c5203033833d001fa18b0578b9
Ivan Guerreschi <ivanguerreschi86@gmail.com>
2019-11-20 16:30:29 +0100

    delete e new webapp Angular

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CredentialListComponent } from './credential-list/credential-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CredentialListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- webapp/src/app/credential.ts
+++
@@ -1,7 +0,0 @@
-export class Credential {
-    id: number;
-    name: string;
-    email: string;
-    password: string;
-    create: Date;
-}
