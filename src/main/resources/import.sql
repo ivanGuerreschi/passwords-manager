e64ef8965134a52299484b3ac43d6a98b789a609
Ivan Guerreschi <ivanguerreschi86@gmail.com>
2019-11-21 15:57:51 +0100

    update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 INSERT INTO credential(id, name, email, password, create) VALUES (nextval('hibernate_sequence'), 'google', 'pippo', 'pluto', CURRENT_DATE);
-INSERT INTO credential(id, name, email, password, create) VALUES (nextval('hibernate_sequence'), 'microsoft', 'pippo', 'pluto', CURRENT_DATE);
-INSERT INTO credential(id, name, email, password, create) VALUES (nextval('hibernate_sequence'), 'twitter', 'pippo', 'pluto', CURRENT_DATE);
+INSERT INTO credential(id, name, email, password, create) VALUES (nextval('hibernate_sequence'), 'microsoft', 'topolino', 'minnie', CURRENT_DATE);
+INSERT INTO credential(id, name, email, password, create) VALUES (nextval('hibernate_sequence'), 'twitter', 'paperino', 'foo', CURRENT_DATE);
