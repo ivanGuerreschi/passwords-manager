0c434e1350881725c5200b2172ca72eede50cb04
Ivan Guerreschi <ivanguerreschi86@gmail.com>
2019-11-21 14:34:05 +0100

    credential list

--- webapp/src/app/credential-list/credential-list.component.ts
+++ webapp/src/app/credential-list/credential-list.component.ts
@@ -1,19 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CredentialService } from '../credential.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-credential-list',
   template: `
-    <p>
-      credential-list works!
-    </p>
+    <div class="panel panel-primary">
+  <div class="panel-heading">
+    <h2>Credential List</h2>
+  </div>
+  <div class="panel-body">
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Password</th>
+          <th>Create</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr *ngFor="let credential of credentials | async">
+          <td>{{credential.id}}</td>
+          <td>{{credential.name}}</td>
+          <td>{{credential.password}</td>
+          <td>{{credential.create}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
   `,
   styles: []
 })
 export class CredentialListComponent implements OnInit {
 
-  constructor() { }
+  credentials: Observable<Credential[]>;
+
+  constructor(private credentialService: CredentialService, private router: Router) { }
 
   ngOnInit() {
+    this.reloadData();
+  }
+
+  reloadData() {
+    this.credentials = this.credentialService.getCredentialsList();
   }
 
 }
--- webapp/src/app/credential.service.ts
+++ webapp/src/app/credential.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CredentialService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api';
+
+  constructor(private http: HttpClient) { }
+
+  getCredential(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  createCredential(credential: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}`, credential);
+  }
+
+  updateCredential(id: number, value: any): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${id}`, value);
+  }
+
+  deleteCredential(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'json' });
+  }
+
+  getCredentialsList(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
 }
